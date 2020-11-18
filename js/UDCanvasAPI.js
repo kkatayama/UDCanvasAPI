fee225475111e8601b0349e6ce6575b12e79d6d3
kkatayama <katayama@udel.edu>
2020-11-17 21:41:56 -0500

    Update UDCanvasAPI.js

--- js/UDCanvasAPI.js
+++ js/UDCanvasAPI.js
@@ -1,4 +1,3 @@
-var access_token = "25~LDtz4FPmgvVumQULtrkjOUrdQvKnniad2r56u33mCOqZHNjnnB2wIkScp8c56F8E";
 var udcanvas = new UDCanvasAPI(access_token).load();
 
 function UDCanvasAPI(access_token) {
